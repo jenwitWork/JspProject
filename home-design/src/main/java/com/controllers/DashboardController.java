eccb1fb8c33866ed1e66bc5d5e732ed5b48641d8
Jenwit Chaneewong <jenwit.work@gmail.com>
2017-09-25 01:39:11 +0700

    home-design.

---
+++ home-design/src/main/java/com/controllers/DashboardController.java
@@ -0,0 +1,15 @@
+package com.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController extends BaseController {
+	
+	@GetMapping("/dashboard")
+	public String dashboard(Model model) {
+		return "dashboard/dashboard";
+	}
+
+}
